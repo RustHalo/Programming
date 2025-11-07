c46735a03c198dfc1765dc2fd3ef44bf20b2f6cb
RustHalo <Adele.Alsaed@Student.HTW-Berlin.de>
2025-11-07 13:34:36 +0100

    Security Check

---
+++ Week4/Failed Login Detector.py
@@ -0,0 +1,23 @@
+login_attempts = [
+("Adele", "success"),
+("Bob", "failed"),
+("Bob", "failed"),
+("Charlie", "success"),
+("Bob", "failed"),
+("Adele", "failed"),
+("Charlie", "failed")]
+
+failed_counts ={}
+
+for username, status in login_attempts:
+    if status == "failed":
+        if username not in failed_counts:
+            failed_counts[username]= 1
+        else:
+            failed_counts [username]+= 1
+
+for username, count in failed_counts.items():
+    if count >=3:
+        print(f"ALERT!: User '{username}' has {count} failed ligin attempts")
+
+print("Security Check complete")
